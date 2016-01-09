3438452e0a29979019451925a3268c64375d373e
chris <chris.bersuch@googlemail.com>
2016-01-09 16:39:38 +0100

    first commit

---
+++ desktop/src/de/norm_fu/icy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.norm_fu.icy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.norm_fu.icy.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ html/src/de/norm_fu/icy/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.norm_fu.icy.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.norm_fu.icy.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
